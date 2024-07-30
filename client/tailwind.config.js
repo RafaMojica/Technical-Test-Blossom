7b29f59d7bc488452bfbe0ee484077a0baef08db
Rafael Mojica <rafael.mojica27@gmail.com>
2024-07-30 15:15:45 -0500

    style: add main colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary100: "#EEE3FF",
+      primary600: "#8054C7",
+      primary700: "#5A3696",
+      secondary600: "#63D838",
+    },
     extend: {},
   },
   plugins: [],
