c4f43e03601ef75e8ca589ff56260f4cfbae6319
Rafael Mojica <rafael.mojica27@gmail.com>
2024-07-31 15:06:08 -0500

    chore: add folder types

---
+++ client/src/types/person.types.ts
@@ -0,0 +1,7 @@
+export interface Person {
+  id: number;
+  name: string;
+  specie: string;
+  image: string;
+  like: boolean;
+}
--- client/src/utils/personsFakeData.ts
+++ client/src/utils/personsFakeData.ts
@@ -1,9 +1,4 @@
-interface Person {
-  id: number;
-  name: string;
-  specie: string;
-  image: string;
-}
+import { Person } from "../types/person.types";
 
 export const personsFakeData: Person[] = [
   {
@@ -11,35 +6,41 @@ export const personsFakeData: Person[] = [
     name: "Abadango Cluster Princess",
     specie: "Alien",
     image: "/img/1.png",
+    like: true,
   },
   {
     id: 2,
     name: "Beth Smith",
     specie: "Human",
     image: "/img/2.png",
+    like: false,
   },
   {
     id: 3,
     name: "Jerry Smith",
     specie: "Human",
     image: "/img/3.png",
+    like: true,
   },
   {
     id: 4,
     name: "Morty Smith",
     specie: "Human",
     image: "/img/4.png",
+    like: false,
   },
   {
     id: 5,
     name: "Rick Sanchez",
     specie: "Human",
     image: "/img/5.png",
+    like: false,
   },
   {
     id: 6,
     name: "Summer Smith",
     specie: "Human",
     image: "/img/6.png",
+    like: false,
   },
 ];
