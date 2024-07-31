a95627e94b694894ed575afe25424413e5131dbc
Rafael Mojica <rafael.mojica27@gmail.com>
2024-07-31 11:10:16 -0500

    chore: creation of false data for layout

---
+++ client/src/utils/personsFakeData.ts
@@ -0,0 +1,45 @@
+interface Person {
+  id: number;
+  name: string;
+  specie: string;
+  image: string;
+}
+
+export const personsFakeData: Person[] = [
+  {
+    id: 1,
+    name: "Abadango Cluster Princess",
+    specie: "Alien",
+    image: "/img/1.png",
+  },
+  {
+    id: 2,
+    name: "Beth Smith",
+    specie: "Human",
+    image: "/img/2.png",
+  },
+  {
+    id: 3,
+    name: "Jerry Smith",
+    specie: "Human",
+    image: "/img/3.png",
+  },
+  {
+    id: 4,
+    name: "Morty Smith",
+    specie: "Human",
+    image: "/img/4.png",
+  },
+  {
+    id: 5,
+    name: "Rick Sanchez",
+    specie: "Human",
+    image: "/img/5.png",
+  },
+  {
+    id: 6,
+    name: "Summer Smith",
+    specie: "Human",
+    image: "/img/6.png",
+  },
+];
