882a02e16329fef09ae103151b58b6d3e2961a9d
Rafael Mojica <rafael.mojica27@gmail.com>
2024-07-31 16:12:01 -0500

    chore: new field added

--- client/src/utils/personsFakeData.ts
+++ client/src/utils/personsFakeData.ts
@@ -6,41 +6,41 @@ export const personsFakeData: Person[] = [
     name: "Abadango Cluster Princess",
     specie: "Alien",
     image: "/img/1.png",
-    like: true,
+    like: true
   },
   {
     id: 2,
     name: "Beth Smith",
     specie: "Human",
     image: "/img/2.png",
-    like: false,
+    like: false
   },
   {
     id: 3,
     name: "Jerry Smith",
     specie: "Human",
     image: "/img/3.png",
-    like: true,
+    like: true
   },
   {
     id: 4,
     name: "Morty Smith",
     specie: "Human",
     image: "/img/4.png",
-    like: false,
+    like: false
   },
   {
     id: 5,
     name: "Rick Sanchez",
     specie: "Human",
     image: "/img/5.png",
-    like: false,
+    like: false
   },
   {
     id: 6,
     name: "Summer Smith",
     specie: "Human",
     image: "/img/6.png",
-    like: false,
+    like: false
   },
 ];
