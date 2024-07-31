8bb4862057d1fc38a629748447f7d2701295c129
Rafael Mojica <rafael.mojica27@gmail.com>
2024-07-31 15:05:46 -0500

    feat: add sort persons

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,17 +1,61 @@
+import { useEffect, useState } from "react";
 import PersonCard from "../common/PersonCard";
 import SearchBar from "../common/SearchBar";
 import { personsFakeData } from "../utils/personsFakeData";
+import { Person } from "../types/person.types";
 
 const Sidebar = () => {
+  const [isSorted, setIsSorted] = useState(true);
+  const [isSortedFavorite, setIsSortedFavorite] = useState(true);
+  const [sortedData, setSortedData] = useState<Person[]>([]);
+  const [sortedFavorite, setSortedFavorite] = useState<Person[]>([]);
+
+  useEffect(() => {
+    const favoriteData = personsFakeData.filter((person) => person.like);
+    const data = personsFakeData.filter((person) => !person.like);
+
+    const sortedData = [...data].sort((a, b) =>
+      isSorted ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+    );
+
+    const sortedFavorite = [...favoriteData].sort((a, b) =>
+      isSortedFavorite
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name)
+    );
+
+    setSortedFavorite(sortedFavorite);
+    setSortedData(sortedData);
+  }, [isSorted, isSortedFavorite]);
+
   return (
     <div>
       <h1 className="text-2xl pb-4">Rick and Morty list</h1>
       <SearchBar />
-      <h2 className="mt-10 py-5 pl-4  text-primaryGrey font-medium">
-        Starred Characters (0)
-      </h2>
-      <h2 className="py-4 pl-4 text-primaryGrey font-medium">Characters (0)</h2>
-      {personsFakeData.map(({ id, name, image, specie }) => (
+
+      <div className="mt-10 flex justify-between items-center text-primaryGrey">
+        <h2 className="py-4 pl-4 font-medium">Starred Characters (0)</h2>
+        <button
+          onClick={() => setIsSortedFavorite(!isSortedFavorite)}
+          className="flex gap-1 hover:text-primaryButton font-medium"
+        >
+          <p>({isSortedFavorite ? "A-Z" : "Z-A"})</p>
+        </button>
+      </div>
+      {sortedFavorite.map(({ id, name, image, specie }) => (
+        <PersonCard key={id} name={name} image={image} specie={specie} />
+      ))}
+
+      <div className="flex justify-between items-center text-primaryGrey">
+        <h2 className="py-4 pl-4 font-medium">Characters (0)</h2>
+        <button
+          onClick={() => setIsSorted(!isSorted)}
+          className="flex gap-1 hover:text-primaryButton font-medium"
+        >
+          <p>({isSorted ? "A-Z" : "Z-A"})</p>
+        </button>
+      </div>
+      {sortedData.map(({ id, name, image, specie }) => (
         <PersonCard key={id} name={name} image={image} specie={specie} />
       ))}
     </div>
