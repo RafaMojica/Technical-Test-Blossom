4b67a47a73759d41a4faae65ef1ecc42cde6a1b0
Rafael Mojica <rafael.mojica27@gmail.com>
2024-08-01 11:16:17 -0500

    feat: add apollo client and initial config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import { ApolloProvider } from "@apollo/client";
+import client from "./config/apollo-client.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
