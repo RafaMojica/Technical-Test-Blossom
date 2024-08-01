f2f6f1b270896bb25de69b8ddf0252242c2f389c
Rafael Mojica <rafael.mojica27@gmail.com>
2024-08-01 12:16:52 -0500

    feat: add show detail persons

--- client/src/components/CharacterDetail.tsx
+++ client/src/components/CharacterDetail.tsx
@@ -1,27 +1,44 @@
 import ButtonLike from "../common/ButtonLike";
 import PersonDescription from "../common/PersonDescription";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_DETAIL_PERSON } from "../services/query/get-detail-person";
+import Loading from "../common/Loading";
 
 const CharacterDetail = () => {
+  const { id } = useParams<{ id: string }>();
+  const { data, loading } = useQuery(GET_DETAIL_PERSON, {
+    variables: { id },
+  });
+
   return (
     <div className="flex flex-col gap-5 flex-grow">
-      <div className="relative flex flex-col gap-2">
-        <img
-          src="/img/1.png"
-          alt="Abadango Cluster Princess"
-          className="h-20 w-20"
-        />
-        <ButtonLike className="absolute left-14 top-12" />
-        <h2 className="font-bold text-2xl">Abadango Cluster Princess</h2>
-      </div>
-      <div>
-        <PersonDescription title="Specie" description="Alien" />
-        <PersonDescription
-          title="Status"
-          description="Alive"
-          className="border-y border-secondaryGrey"
-        />
-        <PersonDescription title="Occupation" description="Princess" />
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="relative flex flex-col gap-2">
+            <img
+              src={data?.character.image}
+              alt="Abadango Cluster Princess"
+              className="h-20 w-20 rounded-full"
+            />
+            <ButtonLike className="absolute left-14 top-12" />
+            <h2 className="font-bold text-2xl">{data?.character.name}</h2>
+          </div>
+          <div>
+            <PersonDescription
+              title="Specie"
+              description={data?.character.species}
+            />
+            <PersonDescription
+              title="Status"
+              description={data?.character.status}
+              className="border-y border-secondaryGrey"
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };
