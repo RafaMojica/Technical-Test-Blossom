f9584d2849dde2c51494199b17d99a333d7f64f0
Rafael Mojica <rafael.mojica27@gmail.com>
2024-07-31 22:42:34 -0500

    feat: created model person postgreSQL whit Sequelize

--- api/server.ts
+++ api/server.ts
@@ -1,17 +1,21 @@
 import express from "express";
 import cors from "cors";
-import db from "./config/db";
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import db from "./config/db/index";
 import models from "./models";
 
+models;
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(cors());
 
-db.sync({ force: false }).then(() => {
-  console.log("DB Connected");
-  app.listen(port, () => {
-    console.log(`Server on http://localhost:${port}`);
+db.sync({ force: false })
+  .then(() => {
+    console.log("DB Connected");
+    app.listen(port, () => {
+      console.log(`Server on http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error syncing the database:", error);
   });
-});
