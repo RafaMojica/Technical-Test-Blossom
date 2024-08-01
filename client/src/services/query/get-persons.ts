1f2353ee7bf9bf6613e690ce95209168f6ea3bc7
Rafael Mojica <rafael.mojica27@gmail.com>
2024-08-01 11:18:38 -0500

    feat: add query GET_PERSONS

---
+++ client/src/services/query/get-persons.ts
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+export const GET_PERSONS = gql`
+  query GetPersons {
+    characters {
+      results {
+        id
+        name
+        species
+        image
+      }
+    }
+  }
+`;
