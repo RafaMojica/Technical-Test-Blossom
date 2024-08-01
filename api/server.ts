4a9a23de3fa96f6f48c6b0f1dc079ff584ca631d
Rafael Mojica <rafael.mojica27@gmail.com>
2024-07-31 21:43:05 -0500

    feat: data base connected

--- api/server.ts
+++ api/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
+import db from "./config/db";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import models from "./models";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Server on http://localhost:${port}`);
+db.sync({ force: false }).then(() => {
+  console.log("DB Connected");
+  app.listen(port, () => {
+    console.log(`Server on http://localhost:${port}`);
+  });
 });
