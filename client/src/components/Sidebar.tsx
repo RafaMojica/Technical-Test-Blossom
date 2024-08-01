472908996d054d648e8da12526a21d2d317c76b0
Rafael Mojica <rafael.mojica27@gmail.com>
2024-08-01 11:19:41 -0500

    feat: change fake data to data api

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,53 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import PersonCard from "../common/PersonCard";
 import SearchBar from "../common/SearchBar";
-import { personsFakeData } from "../utils/personsFakeData";
-import { Person } from "../types/person.types";
+import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_PERSONS } from "../services/query/get-persons";
+import { PersonData } from "../types/person.types";
+import Loading from "../common/Loading";
 
 const Sidebar = () => {
+  const { data, loading } = useQuery<PersonData>(GET_PERSONS);
+
   const [isSorted, setIsSorted] = useState(true);
-  const [isSortedFavorite, setIsSortedFavorite] = useState(true);
-  const [sortedData, setSortedData] = useState<Person[]>([]);
-  const [sortedFavorite, setSortedFavorite] = useState<Person[]>([]);
-
-  useEffect(() => {
-    const favoriteData = personsFakeData.filter((person) => person.like);
-    const data = personsFakeData.filter((person) => !person.like);
-
-    const sortedData = [...data].sort((a, b) =>
-      isSorted ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
-    );
-
-    const sortedFavorite = [...favoriteData].sort((a, b) =>
-      isSortedFavorite
-        ? a.name.localeCompare(b.name)
-        : b.name.localeCompare(a.name)
-    );
-
-    setSortedFavorite(sortedFavorite);
-    setSortedData(sortedData);
-  }, [isSorted, isSortedFavorite]);
 
   return (
     <div>
-      <h1 className="text-2xl pb-4">Rick and Morty list</h1>
+      <h1 className="text-2xl pb-4">
+        <Link to="/">Rick and Morty list</Link>
+      </h1>
       <SearchBar />
 
       <div className="mt-10 flex justify-between items-center text-primaryGrey">
         <h2 className="py-4 pl-4 font-medium">Starred Characters (0)</h2>
-        <button
-          onClick={() => setIsSortedFavorite(!isSortedFavorite)}
-          className="flex gap-1 hover:text-primaryButton font-medium"
-        >
-          <p>({isSortedFavorite ? "A-Z" : "Z-A"})</p>
-        </button>
       </div>
-      {sortedFavorite.map(({ id, name, image, specie }) => (
-        <PersonCard key={id} name={name} image={image} specie={specie} />
-      ))}
 
       <div className="flex justify-between items-center text-primaryGrey">
-        <h2 className="py-4 pl-4 font-medium">Characters (0)</h2>
+        <h2 className="py-4 pl-4 font-medium">
+          Characters {loading ? "(0)" : `(${data?.characters.results.length})`}
+        </h2>
         <button
           onClick={() => setIsSorted(!isSorted)}
           className="flex gap-1 hover:text-primaryButton font-medium"
@@ -55,9 +34,13 @@ const Sidebar = () => {
           <p>({isSorted ? "A-Z" : "Z-A"})</p>
         </button>
       </div>
-      {sortedData.map(({ id, name, image, specie }) => (
-        <PersonCard key={id} name={name} image={image} specie={specie} />
-      ))}
+      {loading ? (
+        <Loading />
+      ) : (
+        data?.characters.results.map(({ id, name, image, species }) => (
+          <PersonCard key={id} name={name} image={image} specie={species} />
+        ))
+      )}
     </div>
   );
 };
