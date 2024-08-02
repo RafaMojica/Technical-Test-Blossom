6e986492b462c95211cb1499772e4b78ba4cc9b4
Rafael Mojica <rafael.mojica27@gmail.com>
2024-08-02 03:55:08 -0500

    feat: creation of comment functionality

--- client/src/components/CharacterDetail.tsx
+++ client/src/components/CharacterDetail.tsx
@@ -5,13 +5,29 @@ import { useMutation, useQuery } from "@apollo/client";
 import { GET_DETAIL_PERSON } from "../services/query/get-detail-person";
 import Loading from "../common/Loading";
 import { TOGGLE_LIKE } from "../services/mutations/toggle-like";
+import FormComment from "../common/FormComment";
+import { GET_COMMENTS_PERSON } from "../services/query/get-comments-person";
+import { Comment } from "../types/comment.types";
 
 const CharacterDetail = () => {
   const [toggleLike] = useMutation(TOGGLE_LIKE);
   const { id } = useParams<{ id: string }>();
+  const personId = Number(id);
   const { data, loading } = useQuery(GET_DETAIL_PERSON, {
     variables: { id },
   });
+
+  const {
+    data: commentsData,
+    loading: commentsLoading,
+    refetch: refetchComments,
+  } = useQuery(GET_COMMENTS_PERSON, {
+    variables: { personId },
+  });
+
+  const handleCommentAdded = async () => {
+    await refetchComments();
+  };
 
   const handleLikeToggle = async () => {
     try {
@@ -55,6 +71,22 @@ const CharacterDetail = () => {
               description={data?.GetDetailPerson.status}
               className="border-y border-secondaryGrey"
             />
+          </div>
+          <FormComment
+            personId={personId}
+            onCommentAdded={handleCommentAdded}
+          />
+          <h3 className="mt-4 font-bold">Comments</h3>
+          <div className="mt-2 border-t">
+            {commentsLoading ? (
+              <Loading />
+            ) : (
+              commentsData?.GetCommentsPerson.map(({ id, text }: Comment) => (
+                <div key={id} className="p-2 border-b last:border-b-0">
+                  <p>{text}</p>
+                </div>
+              ))
+            )}
           </div>
         </div>
       )}
