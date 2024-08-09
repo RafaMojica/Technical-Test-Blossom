73d94b7887dc3f5037bab51cd0a34cb47228a702
Rafael Mojica <rafael.mojica27@gmail.com>
2024-08-09 13:27:17 -0500

    feat: menu filter responsive

--- client/src/components/FilterPanel.tsx
+++ client/src/components/FilterPanel.tsx
@@ -1,16 +1,39 @@
-import { useState } from "react";
+import { FC, useState } from "react";
 import Button from "../common/Button";
 import { FILTER } from "../constants/filter";
 
-const FilterPanel = () => {
-  const [selectedCharacter, setSelectedCharacter] = useState<string>("All");
-  const [selectedSpecie, setSelectedSpecie] = useState<string>("All");
+interface FilterPanelProps {
+  isFilterPanelVisible: boolean;
+  setIsFilterPanelVisible: (isVisible: boolean) => void;
+}
 
-  const isFilterButtonDisabled =
-    selectedCharacter === "All" && selectedSpecie === "All";
+const FilterPanel: FC<FilterPanelProps> = ({
+  isFilterPanelVisible,
+  setIsFilterPanelVisible,
+}) => {
+  const [selectedCharacter, setSelectedCharacter] = useState<string | null>(
+    null
+  );
+  const [selectedSpecie, setSelectedSpecie] = useState<string | null>(null);
+
+  const isFilterButtonDisabled = !selectedCharacter && !selectedSpecie;
 
   return (
-    <div className="flex flex-col gap-6 p-6 bg-white absolute top-28 left-0 right-0 z-10 shadow-xl rounded-xl">
+    <div
+      className="flex flex-col gap-6 bg-white h-full md:h-min
+    md:absolute md:p-6 md:top-28 md:left-0 md:right-0 md:z-10 md:shadow-xl md:rounded-xl
+    "
+    >
+      <div className="relative flex justify-center items-center md:hidden">
+        <button
+          onClick={() => setIsFilterPanelVisible(!isFilterPanelVisible)}
+          className="absolute left-0"
+        >
+          <img src="/icons/Arrow-Left.svg" alt="Arrow left" />
+        </button>
+        <h3 className="m-auto font-medium">Filter</h3>
+      </div>
+
       <div className="flex flex-col gap-2">
         <h3 className="text-primaryGrey font-medium">Character</h3>
         <div className="flex items-center justify-between gap-2">
@@ -39,16 +62,18 @@ const FilterPanel = () => {
           ))}
         </div>
       </div>
-      <Button
-        className={`w-full ${
-          !isFilterButtonDisabled
-            ? "bg-primaryButton text-white"
-            : "bg-tertiaryGrey text-primaryGrey border-tertiaryGrey"
-        }`}
-        disabled={isFilterButtonDisabled}
-      >
-        Filter
-      </Button>
+      <div className="mt-auto">
+        <Button
+          className={`w-full ${
+            !isFilterButtonDisabled
+              ? "bg-primaryButton text-white"
+              : "bg-tertiaryGrey text-primaryGrey border-tertiaryGrey"
+          }`}
+          disabled={isFilterButtonDisabled}
+        >
+          Filter
+        </Button>
+      </div>
     </div>
   );
 };
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -46,37 +46,74 @@ const Sidebar = () => {
   const filteredDataFavorite = filterData(favoriteData, searchTerm);
 
   return (
-    <div className="relative flex flex-col md:h-[calc(100vh-50px)]">
-      <h1 className="text-2xl pb-4">
-        <Link to="/">Rick and Morty list</Link>
-      </h1>
-      <div className="flex flex-col gap-2">
+    <div className="relative flex flex-col h-[calc(100vh-50px)]">
+      {isFilterPanelVisible && (
+        <FilterPanel
+          isFilterPanelVisible={isFilterPanelVisible}
+          setIsFilterPanelVisible={setIsFilterPanelVisible}
+        />
+      )}
+      <div className={`${isFilterPanelVisible && "hidden"} md:block`}>
+        <h1 className="text-2xl pb-4">
+          <Link to="/">Rick and Morty list</Link>
+        </h1>
         <SearchBar
           onSearch={setSearchTerm}
           setIsFilterPanelVisible={setIsFilterPanelVisible}
           isFilterPanelVisible={isFilterPanelVisible}
         />
-        {isFilterPanelVisible && <FilterPanel />}
-      </div>
-      <div className="mt-10 flex-1 flex flex-col md:max-h-[400px]">
-        <div className="flex justify-between items-center p-4 text-primaryGrey font-medium">
-          <h2>Starred Characters ({favoriteData.length})</h2>
-          <button
-            onClick={() => setIsSortedFavorite(!isSortedFavorite)}
-            className="flex hover:text-primaryButton"
-          >
-            <p>({isSortedFavorite ? "A-Z" : "Z-A"})</p>
-          </button>
-          {/* <p className="text-primaryBlue">5 Results</p>
+        <div className="mt-10 flex-1 flex flex-col md:max-h-[400px]">
+          <div className="flex justify-between items-center p-4 text-primaryGrey font-medium">
+            <h2>Starred Characters ({favoriteData.length})</h2>
+            <button
+              onClick={() => setIsSortedFavorite(!isSortedFavorite)}
+              className="flex hover:text-primaryButton"
+            >
+              <p>({isSortedFavorite ? "A-Z" : "Z-A"})</p>
+            </button>
+            {/* <p className="text-primaryBlue">5 Results</p>
           <span className="bg-primaryGreen/20 text-secondaryGreen text-xs px-3 py-0.5 rounded-full">
             1 Filter
           </span> */}
+          </div>
+          <div className="md:overflow-y-scroll">
+            {loading ? (
+              <Loading />
+            ) : (
+              filteredDataFavorite.map(({ id, name, image, species, like }) => (
+                <Link
+                  to={`/person/${id}`}
+                  key={id}
+                  onClick={() => setSelectedId(id)}
+                >
+                  <PersonCard
+                    key={id}
+                    id={id}
+                    name={name}
+                    image={image}
+                    specie={species}
+                    like={like}
+                    isSelected={selectedId === id}
+                  />
+                </Link>
+              ))
+            )}
+          </div>
+        </div>
+        <div className="flex justify-between items-center p-4 text-primaryGrey font-medium">
+          <h2>Characters {loading ? "(0)" : `(${sortedData.length})`}</h2>
+          <button
+            onClick={() => setIsSortedPersons(!isSortedPersons)}
+            className="flex hover:text-primaryButton font-medium"
+          >
+            <p>({isSortedPersons ? "A-Z" : "Z-A"})</p>
+          </button>
         </div>
         <div className="md:overflow-y-scroll">
           {loading ? (
             <Loading />
           ) : (
-            filteredDataFavorite.map(({ id, name, image, species, like }) => (
+            filteredData.map(({ id, name, image, species, like }) => (
               <Link
                 to={`/person/${id}`}
                 key={id}
@@ -95,39 +132,6 @@ const Sidebar = () => {
             ))
           )}
         </div>
-      </div>
-
-      <div className="flex justify-between items-center p-4 text-primaryGrey font-medium">
-        <h2>Characters {loading ? "(0)" : `(${sortedData.length})`}</h2>
-        <button
-          onClick={() => setIsSortedPersons(!isSortedPersons)}
-          className="flex hover:text-primaryButton font-medium"
-        >
-          <p>({isSortedPersons ? "A-Z" : "Z-A"})</p>
-        </button>
-      </div>
-      <div className="md:overflow-y-scroll">
-        {loading ? (
-          <Loading />
-        ) : (
-          filteredData.map(({ id, name, image, species, like }) => (
-            <Link
-              to={`/person/${id}`}
-              key={id}
-              onClick={() => setSelectedId(id)}
-            >
-              <PersonCard
-                key={id}
-                id={id}
-                name={name}
-                image={image}
-                specie={species}
-                like={like}
-                isSelected={selectedId === id}
-              />
-            </Link>
-          ))
-        )}
       </div>
     </div>
   );
